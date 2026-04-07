23c8ef1925ef0691f6e2ee33e61e6e65d89a9c65
Mathew Tyler <MTco@users.noreply.github.com>
2026-04-07 11:59:47 -0700

    Add files via upload

---
+++ NRS/v2/sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'nrs-pro-v2';
+const ASSETS = ['./index.html', './manifest.json', './Tone.14.8.49.min.js'];
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
