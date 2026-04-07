da03b57411ba7ca6cbbc7c5bbfdf3045e600cb0f
Mathew Tyler <MTco@users.noreply.github.com>
2026-04-07 13:10:59 -0700

    Add files via upload

--- VHAS/v2/sw.js
+++ VHAS/v2/sw.js
@@ -1,14 +1,4 @@
-const CACHE_NAME = 'vhas-pro-v2';
-const ASSETS = [
-    './index.html',
-    './manifest.json',
-    './Tone.14.8.49.min.js'
-];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+const CACHE_NAME = 'nrs-pro-v3';
+const ASSETS = ['./index.html', './manifest.json', '../Tone.15.5.6.js'];
+self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))));
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
