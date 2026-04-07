e358694c5864a58e949aafc85efc8df939edeb31
Mathew Tyler <MTco@users.noreply.github.com>
2026-04-07 13:20:58 -0700

    Update service worker cache name and assets

--- VHAS/v2/sw.js
+++ VHAS/v2/sw.js
@@ -1,4 +1,14 @@
-const CACHE_NAME = 'nrs-pro-v3';
-const ASSETS = ['./index.html', './manifest.json', '../Tone.15.5.6.js'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
+const CACHE_NAME = 'vhas-pro-v2';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    '../../Tone.15.5.6.js'
+];
+
+self.addEventListener('install', (e) => {
+	e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
